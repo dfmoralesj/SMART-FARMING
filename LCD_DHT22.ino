91ec1cbfcd27f99e144ca34546439e84b5bb5807
Felipe Jerez <80229497+dfmoralesj@users.noreply.github.com>
2022-03-31 00:04:40 -0500

    Add files via upload

--- LCD_DHT22.ino
+++ LCD_DHT22.ino
@@ -1,7 +1,13 @@
 #include <Wire.h> // 
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <BH1750.h>
+
+BH1750 luxometro(0x23);
+
 LiquidCrystal_I2C lcd(0x27,16,2);//Inicializa pantalla LCD con modulo I2C
+
+
 
 int SENSOR = 2; //Inicializa entero PIN D2
 float temp;
@@ -9,13 +15,18 @@ int humedad, Humedad_Suelo; // Iniciailiza variables tipo entero de temperatura 
 int pinOut = 3; //inicializa variable de salida PIN D3
 
 DHT dht (SENSOR, DHT22);
+BH1750 sensor;
 
 void setup() {
-    lcd.backlight();
+    Serial.begin(9600); //Velocidad de comunicación serial
+
+    
+    lcd.backlight();    //Inicialización luz de fondo LCD
     lcd.init();
     pinMode (pinOut, OUTPUT);
-    
-    Serial.begin(9600);
+
+    sensor.begin();
+    Wire.begin();
     dht.begin();
 }
 
@@ -23,20 +34,32 @@ void loop(){
   humedad = dht.readHumidity(); //humedad del sensor
   temp = dht.readTemperature(); //Temperatura del sensor
   Humedad_Suelo = analogRead(A0); //Humedad de suelo SENSOR 109
- 
+  unsigned lux = sensor.readLightLevel(); //Luminosidad del sensor BH1750
+
+
+//DH22 Sensor de temperatura y humedad ambiente
   Serial.print("Temperatura: "); //Monitor
   Serial.print(temp);
   Serial.println("ºC");
   Serial.print("Humedad: ");//Monitor
   Serial.print(humedad);
   Serial.println("%");
+  
 //DH072 sensor capacitivo de humedad
   Serial.print("Humedad Suelo: ");
   Serial.println(Humedad_Suelo); //Toma de datos del sensor capacitivo
   
-const int SECO = 582; // value for seco sensor Valor 1 de 0% +-5%
-const int HUMEDO = 318; // value for Mojado sensor Valor 2 de 100% +-5%
+//BH1750 sensor de luminosidad 
+  Serial.print(" Luminosidad: ");
+  Serial.print(lux);
+  Serial.println(" Lux ");
+
   
+const int SECO = 590; // Valor para seco sensor (Valor 1) de 0% +-5%
+const int HUMEDO = 318; // Valor para Mojado sensor (Valor) 2 de 100% +-5%
+
+
+//Acople de porcentaje a sensor de humedad de suelo
 int Porcentaje_Humedad_Suelo= map(Humedad_Suelo, HUMEDO, SECO, 100, 0);
   Serial.print("Porcentaje de Humedad Suelo: "); //Monitor
   Serial.println(Porcentaje_Humedad_Suelo);
@@ -56,6 +79,11 @@ if (temp >= 26){              //Condicional para activar salida
   lcd.print(temp);
   lcd.setCursor(7,0); 
   lcd.print("C");
+  //LCD impresion de luminosidad
+  lcd.setCursor(9,0);
+  lcd.print("L: ");
+  lcd.setCursor(11,0);
+  lcd.print(lux);
   //LCD impresion humedad DHT22
   lcd.setCursor(0,1); 
   lcd.print("HAm:"); 
